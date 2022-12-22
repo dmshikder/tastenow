f08d34b4273a95aaf1f86254fb1f208267db5bc2
dmshikder <dmshikder@gmail.com>
2022-12-22 13:17:35 +0600

    categories created

---
+++ src/Assets/SVG/Badge.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Badge = () => {
+  return (
+    <svg width="26" height="37" viewBox="0 0 26 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 37L4.5 0H22L26 37L13 32.5L0 37Z" fill="#F2BC57"/>
+</svg>
+
+  )
+}
+
+export default Badge
--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,11 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Burgers from "../../Assets/SVG/Burgers";
+import Combos from "../../Assets/SVG/Combos";
+import Pizzas from "../../Assets/SVG/Pizzas";
+import Salads from "../../Assets/SVG/Salads";
+import "./Categories.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// import "./styles.css";
+
+// import required modules
+import { FreeMode, Pagination } from "swiper";
+import Badge from "../../Assets/SVG/Badge";
+
 
 const Categories = () => {
-  return (
-    <div>
-    categories
-    </div>
-  )
-}
 
-export default Categories
+  const [items, setItems] = useState([]);
+
+  useEffect(()=>{
+    fetch('fakeData.json')
+    .then(res=>res.json())
+    .then(data=>setItems(data))
+  },[])
+  return (
+    <>
+      <h2>Our Popular Categories</h2>
+      <div className="category-tab-group">
+        <p className="category-tab">
+          <Pizzas />
+          <span className="category-title">Pizzas</span>
+        </p>
+        <p className="category-tab">
+         <Burgers />
+          <span className="category-title">Burgers</span>
+        </p>
+        <p className="category-tab">
+          <Salads/>
+          <span className="category-title">Salads</span>
+        </p>
+        <p className="category-tab">
+          <Combos/>
+          <span className="category-title">Combos</span>
+        </p>
+        
+      </div>
+      <div>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={0}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        {
+          items.map(item=>{
+            return  <SwiperSlide>
+          <div className="category-card">
+          <div className="badge"><Badge/><span className="badge-star">{item.star}</span></div>
+          <img src={item.img} alt="" />
+            <p className="item-name">{item.name}</p>
+            <p>{item.description}</p>
+            <p className="item-price">${item.price}</p>
+          </div>
+        </SwiperSlide>
+          })
+
+        }
+      </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default Categories;
